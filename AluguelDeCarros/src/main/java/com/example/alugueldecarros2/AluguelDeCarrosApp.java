ce85869c952ba0ef10eaa2861eeeb0587e65e0fc
Gabriel PBB <Gabrielpaesbarreto35@gmail.com>
2025-02-19 08:41:24 -0300

    Fazendo sceneManager funcionar na inicialização do app

---
+++ AluguelDeCarros/src/main/java/com/example/alugueldecarros2/AluguelDeCarrosApp.java
@@ -0,0 +1,30 @@
+package com.example.alugueldecarros2;
+
+import com.example.alugueldecarros2.Controllers.SceneManager.SceneManager;
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AluguelDeCarrosApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.setScene(SceneManager.getInstance().getLoginTela());
+        stage.setTitle("Sistema de Aluguel de Carros");
+
+        stage.setWidth(800);
+        stage.setHeight(534);
+        stage.setResizable(false);
+
+        SceneManager.getInstance().setStage(stage);
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
